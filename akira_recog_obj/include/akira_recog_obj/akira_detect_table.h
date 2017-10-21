81470b383e45965009e3575b43d1764c2fcb13bc
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2017-10-21 16:13:19 +0900

    change lib names

--- akira_recog_obj/include/akira_recog_obj/akira_detect_table.h
+++ akira_recog_obj/include/akira_recog_obj/akira_detect_table.h
@@ -1,31 +1,35 @@
-#ifndef AKIRA_RECOG_OBJ_H_DETECT_TABLE_CLASS
-#define AKIRA_RECOG_OBJ_H_DETECT_TABLE_CLASS
+#ifndef AKIRA_RECOG_OBJ_H_RECOG_OBJ_MAIN_CLASS
+#define AKIRA_RECOG_OBJ_H_RECOG_OBJ_MAIN_CLASS
 
 #include <stdio.h>
 #include <ros/ros.h>
+#include <nodelet/nodelet.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/PointCloud.h>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
 
 namespace akira_recog_obj
 {
-  class detectTableClass
+  class detectTableClass : public nodelet::Nodelet
   {
   public:
-    detectTableClass();
-    ~detectTableClass();
+    detectTableClass ();
+    ~detectTableClass ();
 
-    pcl::ModelCoefficients::Ptr coefficients;
-    pcl::PointIndices::Ptr inliers_index;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr inliers_data;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr outliers_data;
-    
-    int presentNumber;
-    int thisIsTable;
+    ros::Publisher pub_obj;
+    ros::Publisher pub_table;
+    ros::Subscriber sub;
 
+    virtual void onInit ();
+    void callback ( const sensor_msgs::PointCloud2::ConstPtr& input );
 
   };
 }
 
 #endif
+
+/*****
+ワンタイムインクルード(インクルードガード)
+ヘッダファイルが複数回インクルードされるのを防ぐ
+
+#ifndef, #define の後の文字列は他のヘッダファイルと重複しないものを指定する．
+2度目以降インクルードされるときは，最初の #ifndef ではじかれる．
+*****/
