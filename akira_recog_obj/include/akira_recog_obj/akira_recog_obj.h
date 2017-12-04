64423c0dcdeeb8d282af64caf1926a532a96a59e
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2017-12-04 15:27:58 +0900

    maybe finish test compilation

--- akira_recog_obj/include/akira_recog_obj/akira_recog_obj.h
+++ akira_recog_obj/include/akira_recog_obj/akira_recog_obj.h
@@ -2,9 +2,29 @@
 #define AKIRA_RECOG_OBJ_H_RECOG_OBJ_MAIN_CLASS
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <ros/ros.h>
+#include <pluginlib/class_list_macros.h>
 #include <nodelet/nodelet.h>
+
 #include <sensor_msgs/PointCloud2.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl_ros/filters/statistical_outlier_removal.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/sac_model_perpendicular_plane.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_ros/filters/filter.h>
 
 namespace akira_recog_obj
 {
@@ -17,9 +37,12 @@ namespace akira_recog_obj
     ros::Publisher pub_grab_position;
     ros::Subscriber sub_raw_clouds;
 
+    bool makeFilter;
+    
     virtual void onInit ();
     void callback ( const sensor_msgs::PointCloud2::ConstPtr& input_clouds );
 
+    void filtering_clouds1 ( const sensor_msgs::PointCloud2::ConstPtr& input_clouds, pcl::PointCloud<pcl::PointXYZ>::Ptr& filtered_clouds );
   };
 }
 
--- akira_recog_obj/src/akira_recog_obj.cpp
+++ akira_recog_obj/src/akira_recog_obj.cpp
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <ros/ros.h>
-#include <pluginlib/class_list_macros.h>
-#include <nodelet/nodelet.h>
-
-#include <sensor_msgs/PointCloud2.h>
-
 #include "akira_recog_obj/akira_recog_obj.h"
 
 namespace akira_recog_obj
@@ -23,14 +14,45 @@ namespace akira_recog_obj
 
   ros::Publisher pub_grab_position;
   ros::Subscriber sub_raw_clouds;
+
+  bool makeFilter;
   
   void recogObjMainClass::onInit ()
   {
     ros::NodeHandle& nh = getNodeHandle ();
-    pub_grag_position = nh.advertise <sensor_msgs::PointCloud2> ( "grab_potision", 1 );
+    pub_grab_position = nh.advertise <sensor_msgs::PointCloud2> ( "grab_potision", 1 );
     sub_raw_clouds = nh.subscribe ( "/camera/depth_registered/points", 10, &recogObjMainClass::callback, this );
+
+    makeFilter = false;
   }
 
   void recogObjMainClass::callback ( const sensor_msgs::PointCloud2::ConstPtr& input_clouds )
   {
+    if ( !makeFilter )
+      {
+	pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_clouds ( new pcl::PointCloud<pcl::PointXYZ> );
     
+	filtering_clouds1 ( input_clouds, filtered_clouds );
+	pub_grab_position.publish ( *filtered_clouds );
+      }
+    else
+      {
+	//
+      }
+  }
+  
+  void recogObjMainClass::filtering_clouds1 ( const sensor_msgs::PointCloud2::ConstPtr& input_clouds, pcl::PointCloud<pcl::PointXYZ>::Ptr& filtered_clouds )
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr noisy_clouds ( new pcl::PointCloud<pcl::PointXYZ> );
+    pcl::fromROSMsg ( *input_clouds, *noisy_clouds );
+    
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    sor.setInputCloud ( noisy_clouds );
+    sor.setMeanK ( 50 );
+    sor.setStddevMulThresh ( 1.0 );
+    sor.filter ( *filtered_clouds );
+  }
+  
+}
+
+PLUGINLIB_EXPORT_CLASS ( akira_recog_obj::recogObjMainClass, nodelet::Nodelet )
