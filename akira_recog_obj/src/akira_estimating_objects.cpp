70ee97635f95f9bbf52396177318443a184500bd
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2018-01-20 20:36:23 +0900

    sdd test code of estimating shape

---
+++ akira_recog_obj/src/akira_estimating_objects.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <visualization_msgs/Marker.h>
+
+namespace akira_recog_obj
+{
+  ros::NodeHandle nh;
+  ros::Subscriber sub_obj_ary;
+  ros::Publisher pub_obj_shape;
+  ros::Publisher pub_obj_line;
+  
+  class estObjClass
+  {
+  public:
+    estObjClass ()
+    {
+      ROS_INFO ( "akira estimating objects node start." );
+      sub_obj_ary = nh.subscribe ( "/tabletop/clusters", 1, &estObjClass::estObjCb, this );
+      pub_obj_shape = nh.advertise <std_msgs::String> ( "object_shape", 1 );
+      pub_obj_line = nh.advertise <visualization_msgs::Marker> ( "object_line", 1 );
+    }
+    ~estObjClass ()
+    {
+      ROS_INFO ( "akira estimating objects node stop." );
+    }
+
+    void estObjCb ( const visualization_msgs::Marker& msg )
+    {
+      ROS_INFO ( "get object" );
+    }
+  };
+}
+
+int main ( int argc, char** argv )
+{
+  ros::init ( argc, argv, "akira_estimating_objects node" );
+  akira_recog_obj::estObjClass obj;
+
+  ros::spin ();
+  return 0;
+}
