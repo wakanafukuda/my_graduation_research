50d57e0690f74834bbe45bb7898623c6331c04fd
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2018-01-22 19:36:19 +0900

    can store data as pointcloud

--- akira_recog_obj/src/akira_estimating_objects.cpp
+++ akira_recog_obj/src/akira_estimating_objects.cpp
@@ -1,8 +1,18 @@
 #include <ros/ros.h>
+
 #include <std_msgs/String.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Pose.h>
 
-#include <sstream>
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+//#include <sstream>
+#include <vector>
 #include <array>
 
 namespace akira_recog_obj
@@ -14,6 +24,7 @@ namespace akira_recog_obj
     ros::Subscriber sub_obj_ary;
     ros::Publisher pub_obj_shape;
     ros::Publisher pub_obj_line;
+    ros::Publisher pub_pcl_obj;
   
     estObjClass ()
     {
@@ -21,25 +32,42 @@ namespace akira_recog_obj
       sub_obj_ary = nh.subscribe ( "/tabletop/clusters", 1, &estObjClass::estObjCb, this );
       pub_obj_shape = nh.advertise <std_msgs::String> ( "object_shape", 1 );
       pub_obj_line = nh.advertise <visualization_msgs::Marker> ( "object_line", 1 );
+      pub_pcl_obj = nh.advertise <sensor_msgs::PointCloud2> ( "object_pcl", 1 );
     }
     ~estObjClass ()
     {
       ROS_INFO ( "akira estimating objects node stop." );
     }
 
-    void estObjCb ( const visualization_msgs::MarkerArray::Ptr& dt )
+    void estObjCb ( const visualization_msgs::MarkerArray::Ptr& raw_data )
     {
+      /*
       std_msgs::String msg;
       std::stringstream ss;
-      ss << dt->markers[ 0 ].header.frame_id;
+      ss << raw_data->markers[ 0 ].header.frame_id;
       msg.data = ss.str ();
       ROS_INFO ( "%s", msg.data.c_str () );
 
-      int length = 0;
-      for ( auto it = std::begin ( dt->markers ) ; it != std::end ( dt->markers ) ; ++it )
+      for ( auto it = std::begin ( raw_data->markers ) ; it != std::end ( raw_data->markers ) ; ++it )
 	{
 	  ROS_INFO ( "%f %f %f", it->points[ 0 ].x, it->points[ 0 ].y, it->points[ 0 ].z );
 	}
+      */
+      std::vector<geometry_msgs::Pose> obj_pose;
+      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_data ( new pcl::PointCloud<pcl::PointXYZ> );
+      for ( auto it = std::begin ( raw_data->markers ) ; it != std::end ( raw_data->markers ) ; ++it )
+	{
+	  obj_pose.push_back ( it->pose );
+	  for ( auto its = std::begin ( it->points ) ; its != std::end ( it->points ) ; ++its )
+	    {
+	      obj_data->push_back ( pcl::PointXYZ ( its->x, its->y, its->z ) );
+	    }
+	}
+      
+      obj_data->header.frame_id = "camera_link";
+      sensor_msgs::PointCloud2::Ptr output ( new sensor_msgs::PointCloud2 );
+      pcl::toROSMsg ( *obj_data, *output );
+      pub_pcl_obj.publish ( *output );
     }
   };
 }
