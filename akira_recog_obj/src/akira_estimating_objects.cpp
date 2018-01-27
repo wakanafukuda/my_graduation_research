94511d7f491349a95bf43039edebcd382247b55f
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2018-01-27 14:14:56 +0900

    just can translate and rotate

--- akira_recog_obj/src/akira_estimating_objects.cpp
+++ akira_recog_obj/src/akira_estimating_objects.cpp
@@ -16,6 +16,10 @@
 #include <sensor_msgs/point_cloud_conversion.h>
 //*** for conversion ***
 
+//*** for affine transform ***
+#include <pcl/common/transforms.h>
+//*** for affine transform ***
+
 //*** for c++ stl ***
 //#include <sstream>
 #include <algorithm>
@@ -25,6 +29,7 @@
 
 //*** for ros::topic class ***
 #include <ros/topic.h>
+//*** for ros::topic class ***
 
 namespace akira_recog_obj
 {
@@ -36,6 +41,7 @@ namespace akira_recog_obj
     ros::Publisher pub_obj_shape;
     ros::Publisher pub_obj_line;
     ros::Publisher pub_pcl_obj;
+    ros::Publisher pub_pcl_trans;
   
     estObjClass ()
     {
@@ -44,6 +50,7 @@ namespace akira_recog_obj
       pub_obj_shape = nh.advertise <std_msgs::String> ( "object_shape", 1 );
       pub_obj_line = nh.advertise <visualization_msgs::Marker> ( "object_line", 1 );
       pub_pcl_obj = nh.advertise <sensor_msgs::PointCloud2> ( "object_pcl", 1 );
+      pub_pcl_trans = nh.advertise <sensor_msgs::PointCloud2> ( "trans_pcl", 1 );
     }
     ~estObjClass ()
     {
@@ -65,10 +72,13 @@ namespace akira_recog_obj
 	}
       */
       
-      object_recognition_msgs::TableArray::ConstPtr table_array = ros::topic::waitForMessage <object_recognition_msgs::TableArray>( "table_array", nh, ros::Duration ( 2.0 ) );
-      geometry_msgs::Pose table_pose = table_array->tables[ 0 ].pose;
-      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_sorted_data ( new pcl::PointCloud<pcl::PointXYZ> );
-      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_data ( new pcl::PointCloud<pcl::PointXYZ> );
+      //object_recognition_msgs::TableArray::ConstPtr table_array = ros::topic::waitForMessage <object_recognition_msgs::TableArray>( "table_array", nh, ros::Duration ( 2.0 ) );
+      //object_recognition_msgs::TableArray::ConstPtr table_array ( new object_recognition_msgs::TableArray );
+      //table_array = ros::topic::waitForMessage <object_recognition_msgs::TableArray> ( "table_array", nh, ros::Duration ( 2.0 ) );
+      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_sorted_data ( new pcl::PointCloud<pcl::PointXYZ> () );
+      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_data ( new pcl::PointCloud<pcl::PointXYZ> () );
+      pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_data ( new pcl::PointCloud<pcl::PointXYZ> () );
+      sensor_msgs::PointCloud2::Ptr transformed_output ( new sensor_msgs::PointCloud2 );
       std::vector<geometry_msgs::Point> temp;
 
       for ( auto it = std::begin ( input_data->markers ) ; it != std::end ( input_data->markers ) ; ++it )
@@ -88,7 +98,7 @@ namespace akira_recog_obj
 	    {
 	      obj_sorted_data->push_back ( pcl::PointXYZ ( its->x, its->y, its->z ) );
 	    }
-	  
+	  /*	  
 	  geometry_msgs::Point& obj_data_max = temp.back ();
 	  geometry_msgs::Point& obj_data_min = temp.front ();
 	  double obj_data_middle1 = ( obj_data_max.z - obj_data_min.z ) / 3.0;
@@ -100,11 +110,30 @@ namespace akira_recog_obj
 		  obj_data->push_back ( pcl::PointXYZ ( its->x, its->y, its->z ) );
 		}
 	    }
+	  */
+	  
+	  object_recognition_msgs::TableArray::ConstPtr table_array = ros::topic::waitForMessage <object_recognition_msgs::TableArray>( "table_array", nh, ros::Duration ( 2.0 ) );
+	  if ( !table_array )
+	    {
+	      ROS_INFO ( "No Table Array" );
+	    }
+	  else
+	    {
+	      geometry_msgs::Pose table_pose = table_array->tables[ 0 ].pose;
+	      float theta = M_PI / 4;
+	      Eigen::Affine3f transform = Eigen::Affine3f::Identity ();
+	      transform.translation () << -table_pose.position.x, -table_pose.position.y, -table_pose.position.z;
+	      transform.rotate ( Eigen::AngleAxisf ( theta, Eigen::Vector3f::UnitY () ) );
+	      pcl::transformPointCloud ( *obj_sorted_data, *transformed_data, transform );
+	      transformed_data->header.frame_id = "camera_llink";
+	      pcl::toROSMsg ( *transformed_data, *transformed_output );
+	      pub_pcl_trans.publish ( *transformed_output );
+	    }
 	}      
       
-      obj_data->header.frame_id = "camera_link";
+      obj_sorted_data->header.frame_id = "camera_link";
       sensor_msgs::PointCloud2::Ptr output ( new sensor_msgs::PointCloud2 );
-      pcl::toROSMsg ( *obj_data, *output );
+      pcl::toROSMsg ( *obj_sorted_data, *output );
       pub_pcl_obj.publish ( *output );
 
       std::vector<geometry_msgs::Point>( ).swap ( temp );
