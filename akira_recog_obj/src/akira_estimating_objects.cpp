a20886c5456ea80d9055e79f3612980739bfa027
wakanafukuda <Wakana.Fukuda@mg4.seikyou.ne.jp>
2018-02-16 02:29:32 +0900

    add new images for presentation and will add vMarker

--- akira_recog_obj/src/akira_estimating_objects.cpp
+++ akira_recog_obj/src/akira_estimating_objects.cpp
@@ -14,6 +14,9 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Pose.h>
 #include <object_recognition_msgs/TableArray.h>
+//#include <std_msgs/MultiArrayLayout.h>
+//#include <std_msgs/MultiArrayDimension.h>
+//#include <std_msgs/Float32MultiArray.h>
 //*** for messageType ***
 
 //*** for conversion *** 
@@ -53,7 +56,6 @@ namespace akira_recog_obj
     double up_y_max, up_y_min;
     double up_diameter, up_radius;
     double x_max, x_min;
-    double depth;
     double z_max, z_min;
     double height;
     double angle;
@@ -73,7 +75,7 @@ namespace akira_recog_obj
       name = "no_name";
       bottom_y_max = -100; bottom_y_min = 100; bottom_diameter = 0;
       up_y_max = -100; up_y_min = 100; up_diameter = 0;
-      x_max = -100; x_min = 100; depth = 0;
+      x_max = -100; x_min = 100;
       z_max = -100; z_min = 100; height = 0;
       angle = 0;
       bottom_center_x = 0; bottom_center_y = 0;
@@ -85,13 +87,12 @@ namespace akira_recog_obj
       bottom_radius = bottom_diameter / 2;
       up_diameter = ( std::abs ( up_y_max - up_y_min ) ) * 100;
       up_radius = up_diameter / 2;
-      depth = ( std::abs ( x_max - x_min ) ) * 100;
       height = ( std::abs ( z_max - z_min ) ) * 100;
       bottom_center_x = up_center_x = ( x_max + x_min ) / 2;
       bottom_center_y = ( bottom_y_max + bottom_y_min ) / 2;
       up_center_y = ( up_y_max + up_y_min ) / 2;
 
-      if ( std::abs ( bottom_diameter - up_diameter ) < 1.0 )
+      if ( std::abs ( bottom_diameter - up_diameter ) <= 1.0 )
 	{
 	  name.erase ( name.begin (), name.end () );
 	  name = "clinder";
@@ -128,8 +129,6 @@ namespace akira_recog_obj
 	    ofs << bottom_diameter << std::endl;
 	  else if ( filename == "/up_diameter.txt" )
 	    ofs << up_diameter << std::endl;
-	  else if ( filename == "/depth.txt" )
-	    ofs << depth << std::endl;
 	  else if ( filename == "/height.txt" )
 	    ofs << height << std::endl;
 	  else if ( filename == "/angle.txt" )
@@ -152,12 +151,12 @@ namespace akira_recog_obj
     //ros::Subscriber sub_obj_ary;
     ros::Subscriber sub_obj_test;
     ros::Publisher pub_pcl_trans;
+    //ros::Publisher pub_data;
     object_data_set obj_data;
     
     std::string dir_name;
     std::string bottom_diameter_file;
     std::string up_diameter_file;
-    std::string depth_file;
     std::string height_file;
     std::string angle_file;
     std::string obj_name_file;
@@ -169,10 +168,10 @@ namespace akira_recog_obj
       //sub_obj_ary = nh.subscribe ( "/tabletop/clusters", 1, &estObjClass::estObjCb, this );
       sub_obj_test = nh.subscribe ( "/akira/tabletop/clusters", 1, &estObjClass::testObjCb, this );
       pub_pcl_trans = nh.advertise <sensor_msgs::PointCloud2> ( "trans_pcl", 1 );
+      //pub_data = nh.advertise <std_msgs::Float32MultiArray> ( "obj_param_data", 1 );
       dir_name = temp_dir_name;
       bottom_diameter_file = "/bottom_diameter.txt";
       up_diameter_file = "/up_diameter.txt";
-      depth_file = "/depth.txt";
       height_file = "/height.txt";
       angle_file = "/angle.txt";
       obj_name_file = "/name.txt";
@@ -214,12 +213,10 @@ namespace akira_recog_obj
 	      obj_input_data->push_back ( pcl::PointXYZ ( its->x, its->y, its->z ) );
 	    }
 
-	  //object_data_set obj_data;
 	  obj_data.size = temp.size ();
 	  std::cout << "size: " << obj_data.size << std::endl;
 	  std::vector<geometry_msgs::Point>( ).swap ( temp );
 	  
-	  //geometry_msgs::Pose table_pose = table_array->tables[ 0 ].pose;
 	  float theta_y = M_PI / 2, theta_z = - M_PI / 2;
 	  Eigen::Affine3f transform = Eigen::Affine3f::Identity ();
 	  transform.rotate ( Eigen::AngleAxisf ( theta_y, Eigen::Vector3f::UnitY () ) );
@@ -259,12 +256,11 @@ namespace akira_recog_obj
 
 	  obj_data.calc_parameters ();
 	  std::cout << "bottom_diameter: " << obj_data.bottom_diameter << ", up_diameter: " << obj_data.up_diameter << std::endl;
-	  std::cout << "depth: " << obj_data.depth << ", height: " << obj_data.height << std::endl;
+	  std::cout << "height: " << obj_data.height << std::endl;
 	  if ( obj_data.name.length () > 0 )
 	    std::cout << "name: " << obj_data.name << ", angle: " << obj_data.angle << std::endl;
 	  obj_data.write_data ( dir_name, bottom_diameter_file );
 	  obj_data.write_data ( dir_name, up_diameter_file );
-	  obj_data.write_data ( dir_name, depth_file );
 	  obj_data.write_data ( dir_name, height_file );
 	  obj_data.write_data ( dir_name, angle_file );
 	  obj_data.write_data ( dir_name, obj_name_file );
@@ -281,51 +277,93 @@ namespace akira_recog_obj
     {
       pcl::PointCloud<pcl::PointXYZ>::Ptr obj_pointcloud ( new pcl::PointCloud<pcl::PointXYZ> );
       pcl::PointCloud<pcl::PointXYZ>::Ptr obj_passthroughed_pointcloud ( new pcl::PointCloud<pcl::PointXYZ> );
-      pcl::PointCloud<pcl::PointXYZ>::Ptr obj_voxelgrid_pointcloud ( new pcl::PointCloud<pcl::PointXYZ> );
+      //pcl::PointCloud<pcl::PointXYZ>::Ptr obj_voxelgrid_pointcloud ( new pcl::PointCloud<pcl::PointXYZ> );
       pcl::fromROSMsg ( *input_data, *obj_pointcloud );
       passthrough_filter ( obj_pointcloud, "z", 0.4, -0.4, obj_passthroughed_pointcloud );
-      voxel_grid ( obj_passthroughed_pointcloud, obj_voxelgrid_pointcloud );
+      //voxel_grid ( obj_passthroughed_pointcloud, obj_voxelgrid_pointcloud );
+      double x_max = -100, x_min = 100;
       for ( pcl::PointCloud<pcl::PointXYZ>::iterator it = obj_passthroughed_pointcloud->begin () ; it != obj_passthroughed_pointcloud->end () ; ++it )
 	{
-	  if ( it->x > obj_data.x_max )
-	    obj_data.x_max = it->x;
-	  else if ( it->x < obj_data.x_min )
-	    obj_data.x_min = it->x;
-	  
+	  if ( it->x > x_max )
+	    x_max = it->x;
+	  if ( it->x < x_min )
+	    x_min = it->x;
 	  if ( it->z > obj_data.z_max )
 	    obj_data.z_max = it->z;
-	  else if ( it->z < obj_data.z_min )
+	  if ( it->z < obj_data.z_min )
 	    obj_data.z_min = it->z;
 	}// for end x_max/min and z_max/min
+      /*
+      double error_range = 0.005;
+      int count_z_size = 0;
+      while ( 1 )
+	{
+	  std::cout << "error range: " << error_range << std::endl;
+	  for ( pcl::PointCloud<pcl::PointXYZ>::iterator it = obj_passthroughed_pointcloud->begin () ; it != obj_passthroughed_pointcloud->end () ; ++it )
+	    {
+	      if ( it->z < ( obj_data.z_min + error_range ) )
+		{
+		  if ( it->y > obj_data.bottom_y_max )
+		    obj_data.bottom_y_max = it->y;
+		  else if ( it->y < obj_data.bottom_y_min )
+		    obj_data.bottom_y_min = it->y;
+		}
+	      else if ( it->z > ( obj_data.z_max - error_range ) )
+		{
+		  if ( it->y > obj_data.up_y_max )
+		    obj_data.up_y_max = it->y;
+		  else if ( it->y < obj_data.up_y_min )
+		    obj_data.up_y_min = it->y;
+		}
+	      ++count_z_size;
+	    }// for end bottom/up
+	  if ( ( count_z_size > 400 ) || ( error_range >= 0.02 ) )
+	    break;
+	  else
+	    {
+	      obj_data.bottom_y_max = 0; obj_data.bottom_y_min = 0;
+	      obj_data.up_y_max = 0; obj_data.up_y_min = 0;
+	      count_z_size = 0;
+	      error_range += 0.005;
+	      continue;
+	    }
+	}// for end while ( 1 )
+      */
       
+      double error_range = 0.01;
       for ( pcl::PointCloud<pcl::PointXYZ>::iterator it = obj_passthroughed_pointcloud->begin () ; it != obj_passthroughed_pointcloud->end () ; ++it )
 	{
-	  if ( it->z < ( obj_data.z_min + 0.02 ) )
+	  if ( it->z < ( obj_data.z_min + error_range ) )
 	    {
 	      if ( it->y > obj_data.bottom_y_max )
 		obj_data.bottom_y_max = it->y;
-	      else if ( it->y < obj_data.bottom_y_min )
+	      if ( it->y < obj_data.bottom_y_min )
 		obj_data.bottom_y_min = it->y;
 	    }
-	  else if ( it->z > ( obj_data.z_max - 0.02 ) )
+	  else if ( it->z > ( obj_data.z_max - error_range ) )
 	    {
 	      if ( it->y > obj_data.up_y_max )
 		obj_data.up_y_max = it->y;
-	      else if ( it->y < obj_data.up_y_min )
+	      if ( it->y < obj_data.up_y_min )
 		obj_data.up_y_min = it->y;
 	    }
 	}// for end bottom/up
+      
+      
       obj_data.size = obj_passthroughed_pointcloud->size ();
 	  
       obj_data.calc_parameters ();
       std::cout << "bottom_diameter: " << obj_data.bottom_diameter << ", up_diameter: " << obj_data.up_diameter << std::endl;
-      std::cout << "depth: " << obj_data.depth << ", height: " << obj_data.height << std::endl;
+      std::cout << "height: " << obj_data.height << std::endl;
       if ( obj_data.name.length () > 0 )
 	std::cout << "name: " << obj_data.name << ", angle: " << obj_data.angle << std::endl;
 
+      double bottom_x_center = ( x_max + x_min ) / 2.0;
+      double bottom_y_center = ( obj_data.bottom_y_max + obj_data.bottom_y_min ) / 2.0;
+      double up_y_center = ( obj_data.up_y_max + obj_data.up_y_min ) / 2.0;
+      
       obj_data.write_data ( dir_name, bottom_diameter_file );
       obj_data.write_data ( dir_name, up_diameter_file );
-      obj_data.write_data ( dir_name, depth_file );
       obj_data.write_data ( dir_name, height_file );
       obj_data.write_data ( dir_name, angle_file );
       obj_data.write_data ( dir_name, obj_name_file );
